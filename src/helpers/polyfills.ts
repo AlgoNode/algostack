d29fddfdf9e9f77b3510fbdb92d2db935fe5ea04
Null <the.email.is.null@gmail.com>
2022-11-01 15:05:01 -0400

    fix displaying nested inner txns

--- src/helpers/polyfills.ts
+++ src/helpers/polyfills.ts
@@ -3,7 +3,7 @@ import { Buffer } from 'buffer';
 export default function polyfills() {
   if (typeof window !== 'undefined') {
     (window as any).global = window;
-    window.Buffer = window.Buffer || Buffer;
+    if(!window.Buffer) window.Buffer = Buffer;
   }
   
   global.Buffer = global.Buffer || Buffer;
